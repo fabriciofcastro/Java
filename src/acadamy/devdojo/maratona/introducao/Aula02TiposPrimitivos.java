f54b13febd891f9ab0fe085d39310fa54cca4b35
fabriciofeitosacastro@gmail.com <fabriciofeitosacastro@gmail.com>
2024-03-22 19:53:42 -0300

    Tipos primitivos em Java parte-2

--- src/acadamy/devdojo/maratona/introducao/Aula02TiposPrimitivos.java
+++ src/acadamy/devdojo/maratona/introducao/Aula02TiposPrimitivos.java
@@ -2,6 +2,16 @@ package acadamy.devdojo.maratona.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
+        // int, double, flot, char, byte, short, long, boolean
 
+        int age = 10;
+        long numberBing = 1000000;
+        double wageDouble = 2000;
+        flot wageFlot = 2000;
+        byte ageByte = -128;
+        short ageShort = 10;
+        boolean isTrue = true;
+        boolean isFalse = false;
+        char caractere = '\u0041';
     }
 }
