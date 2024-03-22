e910cb2567fc4252ec80927a2f0e693acefe689d
fabriciofeitosacastro@gmail.com <fabriciofeitosacastro@gmail.com>
2024-03-22 18:09:58 -0300

    Tipos primitivos em Java

---
+++ src/acadamy/devdojo/maratona/introducao/Aula01OlaDev.java
@@ -0,0 +1,7 @@
+package acadamy.devdojo.maratona.introducao;
+
+public class Aula01OlaDev {
+    public static void main(String[] args) {
+        System.out.println("Fabricio");
+    }
+}
---
+++ src/acadamy/devdojo/maratona/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,7 @@
+package acadamy.devdojo.maratona.introducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+
+    }
+}
