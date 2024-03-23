3bd8f405c0ca8e95affa8e318f42bb1bdb0dd9af
fabriciofeitosacastro@gmail.com <fabriciofeitosacastro@gmail.com>
2024-03-22 22:25:52 -0300

    exercicio de variáveis

--- src/acadamy/devdojo/maratona/introducao/Aula02TiposPrimitivos.java
+++ src/acadamy/devdojo/maratona/introducao/Aula02TiposPrimitivos.java
@@ -2,16 +2,20 @@ package acadamy.devdojo.maratona.introducao;
 
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-        // int, double, flot, char, byte, short, long, boolean
+        // int, double, float, char, byte, short, long, boolean
+        //casting Quando você força o tipo receber quantidade de bytes além de sua capacidade
 
         int age = 10;
         long numberBing = 1000000;
-        double wageDouble = 2000;
-        flot wageFlot = 2000;
+        double wageDouble = 2000.0;
+        float wageFlot = 2000.0F;
         byte ageByte = -128;
         short ageShort = 10;
         boolean isTrue = true;
         boolean isFalse = false;
         char caractere = '\u0041';
+
+        //String não é tipo primitivo. É uma class é portanto começa com letra maiúscula
+        String text = "pode ser texto de qualquer tamanho";
     }
 }
---
+++ src/acadamy/devdojo/maratona/introducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,12 @@
+package acadamy.devdojo.maratona.introducao;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        String name = "Fabrício";
+        String endereco = "Casa no Campo";
+        double salario = 5431.12;
+        String dataRecebimentoSlario = "30/03/2024";
+        String relatorios = "Eu " + name + " morando no endereco " + endereco + "confirmo que recebi o salário de " + salario + ", na data" + dataRecebimentoSlario;
+        System.out.println(relatorios);
+    }
+}
