ab8bbe30b66943b8c9792190ba0a18e232078d92
fabriciofeitosacastro@gmail.com <fabriciofeitosacastro@gmail.com>
2024-03-24 13:48:12 -0300

    Estrutura de arry01

---
+++ src/acadamy/devdojo/maratona/introducao/Aula08Arrays01.java
@@ -0,0 +1,14 @@
+package acadamy.devdojo.maratona.introducao;
+
+public class Aula08Arrays01 {
+    public static void main(String[] args) {
+        int[] idade = new int[3];
+        idade[0] = 21;
+        idade[1] = 30;
+        idade[2] = 45;
+
+        System.out.println(idade[0]);
+        System.out.println(idade[1]);
+        System.out.println(idade[1]);
+    }
+}
