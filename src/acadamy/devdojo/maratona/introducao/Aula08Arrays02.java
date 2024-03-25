e7308251a3176f804a7fc9cfebe46cf56723c6f5
fabriciofeitosacastro@gmail.com <fabriciofeitosacastro@gmail.com>
2024-03-24 22:22:36 -0300

    Estrutura de arry02

---
+++ src/acadamy/devdojo/maratona/introducao/Aula08Arrays02.java
@@ -0,0 +1,15 @@
+package acadamy.devdojo.maratona.introducao;
+
+public class Aula08Arrays02 {
+    public static void main(String[] args) {
+        String[] nome = new String[3];
+        nome[0] = "laura";
+        nome[1] = "Yasmin";
+        nome[2] = "Eloá";
+
+        for (int i = 0; i < nome.length; i++){
+            System.out.println(nome[i]);
+        }
+
+    }
+}
